c82be86fbfd686aeaa243ba285d2aeb062d9cd13
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-10-01 11:23:50 -0300

    Adicionado codigo em teste

---
+++ Codigo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Insira a palavra: ");
+string palavra = Console.ReadLine();
+
+Console.WriteLine("Insira a letra a ser contada: ");
+char letra = char.Parse(Console.ReadLine());
+
+int total =0;
+for(int i = 0; letra < palavra[i]; i++)
+{
+    total++;
+}
+Console.WriteLine($"A letra {letra} foi contada {total} vezes.");
+
+
+// if(int.TryParse(Console.ReadLine(),out int Teste))
+// {
+
+// }
