0f559aa67783235f65b01a71fcdaea6ad0e01697
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-10-02 11:01:12 -0300

    Codigo feito com prints executando

--- Codigo/Program.cs
+++ Codigo/Program.cs
@@ -1,21 +1,71 @@
 ﻿using System.Numerics;
 using System.Reflection.PortableExecutable;
 
-Console.WriteLine("Insira a palavra: ");
-string palavra = Console.ReadLine();
+using System.ComponentModel;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("=".PadLeft(28, '='));
+Console.WriteLine("     Lista de Contatos");
+Console.WriteLine("=".PadLeft(28, '='));
+Console.ResetColor();
 
-Console.WriteLine("Insira a letra a ser contada: ");
-char letra = char.Parse(Console.ReadLine());
+Console.Write("\nQuantos contatos deseja inserir: ");
+int quantidade = int.Parse(Console.ReadLine());
 
-int total =0;
-for(int i = 0; letra < palavra[i]; i++)
+string[] nomes = new string[quantidade];
+
+for (int i = 0; i < quantidade; i++)
 {
-    total++;
+    Console.Write($"\nDigite o contato {i + 1}: ");
+    nomes[i] = Console.ReadLine().ToLower();
 }
-Console.WriteLine($"A letra {letra} foi contada {total} vezes.");
 
+List<string> lista = new List<string>(nomes);
 
-// if(int.TryParse(Console.ReadLine(),out int Teste))
-// {
+Console.Write("\nQuer remover algum contato?: ");
+string segue = Console.ReadLine().ToLower();
 
-// }
+if (segue == "sim".ToLower() || segue == "s".ToLower())
+{
+
+    Console.Write("\nDigite o nome do contato que deseja remover: ");
+    string valorRemover = Console.ReadLine().ToLower();
+
+    if (lista.Remove(valorRemover))
+    {
+        Console.WriteLine($"\n{valorRemover} foi removido com sucesso");
+    }
+    else
+    {
+        Console.WriteLine($"\n{valorRemover} não foi encontrado");
+    }
+
+    nomes = lista.ToArray();
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("=".PadLeft(32, '='));
+   Console.WriteLine("\nLista de contatos após remoção: ");
+    Console.WriteLine("=".PadLeft(32, '='));
+    
+    foreach (string nome in nomes)
+    {
+        Console.WriteLine(nome);
+         Console.WriteLine("-".PadLeft(10, '-'));
+    }
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("=".PadLeft(32, '='));
+    Console.WriteLine("\nAgenda de Contatos Salvos\n");
+    Console.WriteLine("=".PadLeft(32, '='));
+    foreach (string nome in nomes)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine("-".PadLeft(10, '-'));
+        
+    }
+}
+Console.ResetColor();
+
