7ecf1126f372685609aebe730038c2f3adf6a604
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2024-10-03 08:36:53 -0300

    Adicionado alteraçoes e adicionado readme

--- Codigo/Program.cs
+++ Codigo/Program.cs
@@ -1,72 +1,80 @@
 ﻿using System.Numerics;
 using System.Reflection.PortableExecutable;
-
 using System.ComponentModel;
+using System.IO;
 Console.Clear(); //Limpa qualquer mensagem de erro ou aviso antes do código começar
-
-Console.ForegroundColor = ConsoleColor.DarkGreen; //Pinta uma parte do código para verde escuro
-Console.WriteLine("=".PadLeft(28, '=')); //Cria um cabeçalho
-Console.WriteLine("     Lista de Contatos"); //Escreve na tela 
-Console.WriteLine("=".PadLeft(28, '=')); //Cria um cabeçalho
-Console.ResetColor(); //Reseta a cor criada 
-
-Console.Write("\nQuantos contatos deseja inserir: "); // Escreve uma mensagem na tela para o usuário inserir a quantidade de contatos
-int quantidade = int.Parse(Console.ReadLine()); //Inseri a quantidade convertendo para número
-
-string[] nomes = new string[quantidade]; //Cria um array de nomes para a quantidade digitada
-
-for (int i = 0; i < quantidade; i++) //Estrutura de repetição para a quantidade de numeros nao passar a variavel contabil
-{
-    Console.Write($"\nDigite o contato {i + 1}: "); // Vai aumentando o número de Contatos sem ultrapassar a informada pelo usuário
-    nomes[i] = Console.ReadLine().ToLower(); //Lê a entrada do usuário e armazena no array nomes
-}
-
-List<string> lista = new List<string>(nomes); //Converte o array para uma lista para poder remover algum valor dentro do array
-
-Console.Write("\nQuer remover algum contato?: "); //Pergunta se o usuário quer remover algum contato
-string segue = Console.ReadLine().ToLower(); //Usuário informa com sim ou não se quer remover algum contato
-
-if (segue == "sim".ToLower() || segue == "s".ToLower()) //Se for inserido "sim" ou "s" entra no bloco de código
+using (StreamWriter escrever = new StreamWriter("lista-de-contatos.txt"))
 {
 
-    Console.Write("\nDigite o nome do contato que deseja remover: "); //Pede para o usuário inserir o nome do contato que deseja remover
-    string valorRemover = Console.ReadLine().ToLower(); //Usuário informa o nome do contato
+    Console.ForegroundColor = ConsoleColor.DarkGreen; //Pinta uma parte do código para verde escuro
+    Console.WriteLine("=".PadLeft(28, '=')); //Cria um cabeçalho
+    Console.WriteLine("     Lista de Contatos"); //Escreve na tela 
+    Console.WriteLine("=".PadLeft(28, '=')); //Cria um cabeçalho
+    Console.ResetColor(); //Reseta a cor criada 
 
-    if (lista.Remove(valorRemover)) //Remove o valor da lista
+    Console.Write("\nQuantos contatos deseja inserir: "); // Escreve uma mensagem na tela para o usuário inserir a quantidade de contatos
+    int quantidade = int.Parse(Console.ReadLine()); //Inseri a quantidade convertendo para número
+
+    string[] nomes = new string[quantidade]; //Cria um array de nomes para a quantidade digitada
+
+    for (int i = 0; i < quantidade; i++) //Estrutura de repetição para a quantidade de numeros nao passar a variavel contabil
     {
-        Console.WriteLine($"\n{valorRemover} foi removido com sucesso");
+        Console.Write($"\nDigite o contato {i + 1}: "); // Vai aumentando o número de Contatos sem ultrapassar a informada pelo usuário
+        nomes[i] = Console.ReadLine().ToLower(); //Lê a entrada do usuário e armazena no array nomes
     }
-    else // senão existe o nome ele nao é encontrado
+
+    List<string> lista = new List<string>(nomes); //Converte o array para uma lista para poder remover algum valor dentro do array
+
+    Console.Write("\nQuer remover algum contato?: "); //Pergunta se o usuário quer remover algum contato
+    string segue = Console.ReadLine().ToLower(); //Usuário informa com sim ou não se quer remover algum contato
+
+    if (segue == "sim".ToLower() || segue == "s".ToLower()) //Se for inserido "sim" ou "s" entra no bloco de código
     {
-        Console.WriteLine($"\n{valorRemover} não foi encontrado");
+
+        Console.Write("\nDigite o nome do contato que deseja remover: "); //Pede para o usuário inserir o nome do contato que deseja remover
+        string valorRemover = Console.ReadLine().ToLower(); //Usuário informa o nome do contato
+
+        if (lista.Remove(valorRemover)) //Remove o valor da lista
+        {
+            Console.WriteLine($"\n{valorRemover} foi removido com sucesso");
+        }
+        else // senão existe o nome ele nao é encontrado
+        {
+            Console.WriteLine($"\n{valorRemover} não foi encontrado");
+        }
+
+        nomes = lista.ToArray(); //Trasnforma a lista em array novamente
+
+        Console.ForegroundColor = ConsoleColor.Green; //Pinta uma parte do código para verde
+        Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
+        Console.WriteLine("\nLista de contatos após remoção: ");
+        escrever.WriteLine("\nLista de contatos após remoção: ");
+        Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
+
+        foreach (string nome in nomes) //Estrutura de repetição, procura nome dentro de nomes
+        {
+            Console.WriteLine(nome); //Escreve a lista de nome
+            escrever.WriteLine(nome); // Escreve no bloco de notas
+            Console.WriteLine("-".PadLeft(10, '-')); //Cria um cabeçalho
+        }
+        Console.ResetColor(); //Reseta a cor
     }
-
-    nomes = lista.ToArray(); //Trasnforma a lista em array novamente
-
-    Console.ForegroundColor = ConsoleColor.Green; //Pinta uma parte do código para verde
-    Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
-    Console.WriteLine("\nLista de contatos após remoção: "); 
-    Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
-
-    foreach (string nome in nomes) //Estrutura de repetição, procura nome dentro de nomes
+    else
     {
-        Console.WriteLine(nome); //Escreve a lista de nome
-        Console.WriteLine("-".PadLeft(10, '-')); //Cria um cabeçalho
+        Console.ForegroundColor = ConsoleColor.Green; //Pinta uma parte do código para verde
+        Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
+        Console.WriteLine("\nAgenda de Contatos Salvos\n");
+        escrever.WriteLine("\nAgenda de Contatos Salvos\n");
+        Console.WriteLine("=".PadLeft(32, '='));  //Cria um cabeçalho
+        foreach (string nome in nomes) //Estrutura de repetição, procura nome dentro de nomes
+        {
+            Console.WriteLine(nome); //Escreve a lista de nome
+            escrever.WriteLine(nome); // Escreve no bloco de notas
+            Console.WriteLine("-".PadLeft(10, '-')); //Cria um cabeçalho
+
+        }
     }
     Console.ResetColor(); //Reseta a cor
+    Console.WriteLine("Lista de contatos geradas e salvas no arquivo lista-de-contatos.txt.\n");
 }
-else
-{
-    Console.ForegroundColor = ConsoleColor.Green; //Pinta uma parte do código para verde
-    Console.WriteLine("=".PadLeft(32, '=')); //Cria um cabeçalho
-    Console.WriteLine("\nAgenda de Contatos Salvos\n");
-    Console.WriteLine("=".PadLeft(32, '='));  //Cria um cabeçalho
-    foreach (string nome in nomes) //Estrutura de repetição, procura nome dentro de nomes
-    {
-        Console.WriteLine(nome); //Escreve a lista de nome
-        Console.WriteLine("-".PadLeft(10, '-')); //Cria um cabeçalho
-
-    }
-}
-Console.ResetColor(); //Reseta a cor
 
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # ParaMeuCurriculo
+
+Aluno: Luiz Eduardo Pereira 
+
+## O que foi utilizado
+
+* Variáveis
+
+* Array
+
+* For e foreach
+
+* if e else 
+
+* List <T>
+
+## Etapas implementadas
+
+* Pede ao usuário que insira a quantidade de contatos.
+![alt text](1.PNG)
+
+* Usuário insere a quantidade de contatos que deseja e os nomes dos contatos ja tendo a opção de remover os contatos caso seja da vontade do usuário
+![alt text](2.PNG)
+
+* Usuário inseri os nomes que deseja remover e os nomes são imprimidos na tela e salvos em um arquivo .txt
+![alt text](3-1.PNG)
+
+* Caso não queira remover nenhum contato e imprmido a lista de contato e salva no arquivo .txt 
+![alt text](4.PNG)
+
+## Backlog
+
+* Futuras implementações :
+
+1. Usuário possa inserir número para o contato
+2. Nome dos contatos possam ser apagados sem precisar de conversão para nomes minúsculos.
+
+## Conclusão
+
+Resultado simples porém código foi difícil para criar devido a nova variáveis e implantação do array ler a entrada do usuário
+
+# Prints do Código
+
+![alt text](5-1.PNG)
+
+![alt text](6.PNG)
+
+![alt text](7.PNG)
